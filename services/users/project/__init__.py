f0fbcc892b28d18de14db925890a81bbe15b2974
nicolegg <estefannygarcia@upeu.edu.pe>
2020-10-26 22:35:03 -0500

    Formulario-componentes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -6,11 +6,13 @@ import os
 from flask import Flask  # nuevo
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension 
+from flask_cors import CORS  # nuevo
 
 
 # instantiate the db
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
+cors = CORS()  # nuevo
 
 
 # nuevo
@@ -26,6 +28,7 @@ def create_app(script_info=None):
     # configurar la extension
     db.init_app(app)
     toolbar.init_app(app)
+    cors.init_app(app)
 
     # registrar blueprints
     from project.api.users import users_blueprint
